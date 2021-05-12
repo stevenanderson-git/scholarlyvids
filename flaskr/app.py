530fcb1ed52bbf5c28e3664f1dba9398abd30940
stevenanderson-git <sdeananderson@gmail.com>
2021-05-12 19:08:57 -0400

    initial bootstrap and beginning of css changes

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,12 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, Flask!"
+    title = 'ScholarlyVids - The best place to rate educational videos!'
+    return render_template('home.html', title = title)
 
+@app.route("/page1")
+def page1():
+    return render_template('page1.html', title = 'page1')
 
+@app.route("/page2")
+def page2():
+    return render_template('page2.html')
 
 
 
